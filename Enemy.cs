657da257c147b89621c3fea0b173d1fd13c7c56c
chorga <kpolucas88@gmail.com>
2019-10-19 16:27:23 -0300

    adaptado para las animaciones

--- Enemy.cs
+++ Enemy.cs
@@ -8,10 +8,13 @@ public class Enemy : MonoBehaviour
     float speed = 0.4f;
     int currentHealth = 2;
     Renderer rend;
+    Animator animator;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
+        animator = GetComponent<Animator>();
+        Physics2D.IgnoreLayerCollision(8,8);
     }
 
     void Update()
@@ -21,7 +24,7 @@ public class Enemy : MonoBehaviour
 
     void Move()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 
     public void Damage()
@@ -29,20 +32,32 @@ public class Enemy : MonoBehaviour
         currentHealth--;
         if (currentHealth <= 0)
         {
-            Destroy(gameObject); // falta agregar animacion
+            StartCoroutine(DeathAnimation());
         }
         else
         {
-            StartCoroutine(ChangeColor()); // falta agregar animacion
+            StartCoroutine(DamageAnimation());
         }
     }
 
 
-    IEnumerator ChangeColor()
+
+
+    IEnumerator DamageAnimation()
     {
         var originalColor = rend.material.color;
-        rend.material.color = Color.red;
+        rend.material.color = new Color(1f, 0.7f, 0.7f, 1f);
+        animator.SetBool("damaged", true);
         yield return new WaitForSeconds(0.5f);
         rend.material.color = originalColor;
+        animator.SetBool("damaged", false);
+    }
+
+    IEnumerator DeathAnimation()
+    {
+        speed = 0f;
+        animator.SetBool("death", true);
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
     }
 }
